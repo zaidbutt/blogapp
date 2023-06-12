cd6b59d3665a67c6b5e866da2ace0a528caff534
zaidbutt <zaidnadeem0604@gmail.com>
2023-06-12 10:44:55 -0700

    BLOG-001 Moving From default SQLite to Postgres

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'blogapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+
+#     }
+# }
+DATABASES = {}
+DATABASES = { 'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'blog-app-db',
+    'USER': 'yomolopo',
+    'PASSWORD': 'yomolopo',
+    'HOST': '127.0.0.1',
+    'PORT': '5432'
     }
 }
+# DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 
 # Password validation
