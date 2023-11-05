abe53550600b43a30e73387fafb39a2994585a62
zaidbutt <zaidnadeem0604@gmail.com>
2023-11-05 15:19:59 +1100

    BLOG-002 Add Post Model

---
+++ blogapp/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'DRAFT'
+        PUBLISHED = 'PB', 'PUBLISHED'
+
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    status = models.CharField(max_length=2, 
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+
+    def __str__(self):
+        return self.title
